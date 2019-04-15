995fddd768401ea9b6aad104ea3965741d73c020
Caleb Sides <calebsides@LPATLICLLOAN13M.local>
2019-04-15 12:57:30 -0400

    changed jenkinsfile to point to dockerfile.prod

--- src/redux/actions/set_smart.js
+++ src/redux/actions/set_smart.js
@@ -1,25 +1,26 @@
-import { SET_SMART_SUCCESS } from "../actions/types";
+import { SET_SMART_SUCCESS } from '../actions/types';
 
 export const setSmart = () => dispatch => {
-    return new Promise((resolve, reject) => {
-        let tmp_smart = null;
+  return new Promise((resolve, reject) => {
+    let tmp_smart = null;
 
-        if (process.env.NODE_ENV === "development") {
-            tmp_smart = window.FHIR.client({
-                serviceUrl: process.env.REACT_APP_DEFAULT_FHIR_URL,
-                patientId: "14628"
-            });
-        } else {
-            if (window.FHIR) {
-                window.FHIR.oauth2.ready(smart => {
-                    tmp_smart = smart;
-                });
-            }
-        }
-        dispatch({
-            type: SET_SMART_SUCCESS,
-            data: tmp_smart
+    if (process.env.NODE_ENV === 'development') {
+      tmp_smart = window.FHIR.client({
+        serviceUrl: process.env.REACT_APP_DEFAULT_FHIR_URL,
+        patientId: '14628'
+      });
+    } else {
+      if (window.FHIR) {
+        window.FHIR.oauth2.ready(smart => {
+          tmp_smart = smart;
         });
-        resolve(tmp_smart);
+      }
+    }
+
+    dispatch({
+      type: SET_SMART_SUCCESS,
+      data: tmp_smart
     });
+    resolve(tmp_smart);
+  });
 };
