0a397dbc6054ab2de50335a662de4fbedfa3dc75
Caleb Sides <calebsides@LPATLICLLOAN13M.local>
2019-04-15 13:13:06 -0400

    trying to get smart working in prod

--- src/redux/actions/set_smart.js
+++ src/redux/actions/set_smart.js
@@ -4,18 +4,18 @@ export const setSmart = () => dispatch => {
   return new Promise((resolve, reject) => {
     let tmp_smart = null;
 
-    if (process.env.NODE_ENV === 'development') {
-      tmp_smart = window.FHIR.client({
-        serviceUrl: process.env.REACT_APP_DEFAULT_FHIR_URL,
-        patientId: '14628'
+    // if (process.env.NODE_ENV === 'development') {
+    //   tmp_smart = window.FHIR.client({
+    //     serviceUrl: process.env.REACT_APP_DEFAULT_FHIR_URL,
+    //     patientId: '14628'
+    //   });
+    // } else {
+    if (window.FHIR) {
+      window.FHIR.oauth2.ready(smart => {
+        tmp_smart = smart;
       });
-    } else {
-      if (window.FHIR) {
-        window.FHIR.oauth2.ready(smart => {
-          tmp_smart = smart;
-        });
-      }
     }
+    // }
 
     dispatch({
       type: SET_SMART_SUCCESS,
