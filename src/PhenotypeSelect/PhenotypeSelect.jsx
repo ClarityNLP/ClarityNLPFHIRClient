182b9a9b626586b3cffa2dbead5f63afc0dc8db1
Caleb Sides <calebsides@LPATLICLLOAN13M.local>
2019-04-15 11:27:23 -0400

    finalizing with correct nlpaas url

--- src/PhenotypeSelect/PhenotypeSelect.jsx
+++ src/PhenotypeSelect/PhenotypeSelect.jsx
@@ -1,159 +1,161 @@
-import React, { Component } from "react";
-import { Container, Row, Col, Button, Alert } from "reactstrap";
-import prettify from "../utils/prettify";
-import Select from "react-select";
+import React, { Component } from 'react';
+import { Container, Row, Col, Button, Alert } from 'reactstrap';
+import prettify from '../utils/prettify';
+import Select from 'react-select';
 
 const initialState = {
-    categoryOptions: [],
-    categorySelections: [],
-    phenotypeOptions: [],
-    phenotypeSelections: []
+  categoryOptions: [],
+  categorySelections: [],
+  phenotypeOptions: [],
+  phenotypeSelections: []
 };
 
 export default class PhenotypeSelect extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = initialState;
+    this.state = initialState;
+  }
+
+  componentDidMount() {
+    const { library } = this.props.app;
+    const tmp_categoryOptions = [];
+
+    for (let prop in library) {
+      if (library.hasOwnProperty(prop)) {
+        const prettyProp = prettify(prop, true);
+
+        tmp_categoryOptions.push({
+          value: prop,
+          label: prettyProp
+        });
+      }
     }
 
-    componentDidMount() {
-        const { library } = this.props.app;
-        const tmp_categoryOptions = [];
+    this.setState(
+      {
+        categoryOptions: tmp_categoryOptions,
+        categorySelections: tmp_categoryOptions
+      },
+      this.setPhenotypeOptions
+    );
+  }
 
-        for (let prop in library) {
-            if (library.hasOwnProperty(prop)) {
-                const prettyProp = prettify(prop, true);
+  setPhenotypeOptions = () => {
+    const { library } = this.props.app;
+    const { categorySelections } = this.state;
+    let tmp_phenotypeOptions = [];
 
-                tmp_categoryOptions.push({
-                    value: prop,
-                    label: prettyProp
-                });
-            }
-        }
+    for (let index in categorySelections) {
+      const category = categorySelections[index].value;
+      let tmp = [];
 
-        this.setState(
-            {
-                categoryOptions: tmp_categoryOptions,
-                categorySelections: tmp_categoryOptions
+      tmp = library[category].map(value => {
+        if (value) {
+          const prettyValue = prettify(value, true);
+
+          return {
+            value: {
+              task: value,
+              category: category
             },
-            this.setPhenotypeOptions
-        );
-    }
-
-    setPhenotypeOptions = () => {
-        const { library } = this.props.app;
-        const { categorySelections } = this.state;
-        let tmp_phenotypeOptions = [];
-
-        for (let index in categorySelections) {
-            const category = categorySelections[index].value;
-            let tmp = [];
-
-            tmp = library[category].map(value => {
-                const prettyValue = prettify(value, true);
-
-                return {
-                    value: {
-                        task: value,
-                        category: category
-                    },
-                    label: prettyValue
-                };
-            });
-
-            tmp_phenotypeOptions.push(tmp);
+            label: prettyValue
+          };
         }
 
-        this.setState({
-            phenotypeOptions: tmp_phenotypeOptions.flat()
-        });
-    };
+        return {};
+      });
 
-    handleCategorySelect = value => {
-        this.setState(
-            {
-                categorySelections: value
-            },
-            this.setPhenotypeOptions
-        );
-    };
-
-    handlePhenotypeSelect = value => {
-        this.setState({
-            phenotypeSelections: value
-        });
-    };
-
-    handleRunClick = () => {
-        const { patient } = this.props.app;
-        const { phenotypeSelections } = this.state;
-
-        const selections = phenotypeSelections.map(value => {
-            return value.value;
-        });
-
-        this.props.setSelections(selections, patient);
-
-        if (selections.length > 0 && patient !== {} && patient.documents) {
-            this.props.setResults(selections, patient);
-            this.props.history.push("/results");
-        }
-    };
-
-    render() {
-        const { results_error } = this.props.app;
-        const {
-            categoryOptions,
-            categorySelections,
-            phenotypeOptions,
-            phenotypeSelections
-        } = this.state;
-
-        return (
-            <Container>
-                <Row className="justify-content-center align-items-center">
-                    <Col md="8">
-                        <Row className="justify-content-end">
-                            {results_error === "" ? null : (
-                                <Col xs="12" className="mb-3">
-                                    <Alert color="danger">
-                                        {results_error}
-                                    </Alert>
-                                </Col>
-                            )}
-                            <Col xs="12" className="mb-3">
-                                <Select
-                                    isMulti={true}
-                                    value={categorySelections}
-                                    onChange={this.handleCategorySelect}
-                                    options={categoryOptions}
-                                    placeholder="Select Categories..."
-                                />
-                            </Col>
-                            <Col xs="12" className="mb-3">
-                                <Select
-                                    isMulti={true}
-                                    value={phenotypeSelections}
-                                    onChange={this.handlePhenotypeSelect}
-                                    options={phenotypeOptions}
-                                    placeholder="Select Phenotypes..."
-                                />
-                            </Col>
-                            <Col md="4" className="mb-3">
-                                <Button
-                                    block
-                                    outline
-                                    color="primary"
-                                    onClick={this.handleRunClick}
-                                >
-                                    RUN
-                                </Button>
-                            </Col>
-                        </Row>
-                    </Col>
-                </Row>
-            </Container>
-        );
+      tmp_phenotypeOptions.push(tmp);
     }
+
+    this.setState({
+      phenotypeOptions: tmp_phenotypeOptions.flat()
+    });
+  };
+
+  handleCategorySelect = value => {
+    this.setState(
+      {
+        categorySelections: value
+      },
+      this.setPhenotypeOptions
+    );
+  };
+
+  handlePhenotypeSelect = value => {
+    this.setState({
+      phenotypeSelections: value
+    });
+  };
+
+  handleRunClick = () => {
+    const { patient } = this.props.app;
+    const { phenotypeSelections } = this.state;
+
+    const selections = phenotypeSelections.map(value => {
+      return value.value;
+    });
+
+    this.props.setSelections(selections, patient);
+
+    if (selections.length > 0 && patient !== {} && patient.documents) {
+      this.props.setResults(selections, patient);
+      this.props.history.push('/results');
+    }
+  };
+
+  render() {
+    const { results_error } = this.props.app;
+    const {
+      categoryOptions,
+      categorySelections,
+      phenotypeOptions,
+      phenotypeSelections
+    } = this.state;
+
+    return (
+      <Container>
+        <Row className='justify-content-center align-items-center'>
+          <Col md='8'>
+            <Row className='justify-content-end'>
+              {results_error === '' ? null : (
+                <Col xs='12' className='mb-3'>
+                  <Alert color='danger'>{results_error}</Alert>
+                </Col>
+              )}
+              <Col xs='12' className='mb-3'>
+                <Select
+                  isMulti={true}
+                  value={categorySelections}
+                  onChange={this.handleCategorySelect}
+                  options={categoryOptions}
+                  placeholder='Select Categories...'
+                />
+              </Col>
+              <Col xs='12' className='mb-3'>
+                <Select
+                  isMulti={true}
+                  value={phenotypeSelections}
+                  onChange={this.handlePhenotypeSelect}
+                  options={phenotypeOptions}
+                  placeholder='Select Phenotypes...'
+                />
+              </Col>
+              <Col md='4' className='mb-3'>
+                <Button
+                  block
+                  outline
+                  color='primary'
+                  onClick={this.handleRunClick}
+                >
+                  RUN
+                </Button>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
